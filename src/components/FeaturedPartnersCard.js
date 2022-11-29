3b04fef9f2510e2be03c034077289c991835ce69
travis flake <tflake83@gmail.com>
2022-11-29 06:18:25 -0500

    added rating

--- src/components/FeaturedPartnersCard.js
+++ src/components/FeaturedPartnersCard.js
@@ -1,22 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'react-bootstrap/Image'
 import Card from 'react-bootstrap/Card';
-import { AiOutlineStar } from 'react-icons/ai';
+import GetRating from './GetRating';
 
-export default function FeaturedPartnersCard({ photo, rating, brandName, id }) {
+export default function FeaturedPartnersCard({ photo, ratingsArray, brandName, id }) {
 
   return (
     <div>
       <Card style={{ width: '18rem' }} className='m-4'>
         <Card.Body className='grid justify-items-center'>
-            <Image src={photo} fluid />
+          <Image src={photo} className='h-60' />
           <Card.Title>{brandName}</Card.Title>
-          <div className='flex'>
-            <AiOutlineStar className='text-yellow-500'/>
-            <AiOutlineStar className='text-yellow-500' />
-            <AiOutlineStar className='text-yellow-500'/>
-            <AiOutlineStar />
-            <AiOutlineStar />
+          <div>
+            <GetRating ratingsArray={ratingsArray}/>
           </div>
         </Card.Body>
       </Card>
