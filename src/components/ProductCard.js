96307e7d7eb6039cfa5e7dac0ef1c9bbdccc3b2f
travis flake <tflake83@gmail.com>
2022-11-29 06:22:04 -0500

    css change

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,7 @@ import Image from 'react-bootstrap/Image'
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from 'react-router-dom'
 
-export default function ProductCard({ photo, brand, description, price }) {
+export default function ProductCard({ photo, brand, description, price, productId }) {
     const navigate = useNavigate()
 
     return (
@@ -13,13 +13,14 @@ export default function ProductCard({ photo, brand, description, price }) {
                     photo:photo,
                     brand: brand,
                     description: description,
-                    price: price
+                    price: price,
+                    productId: productId
                 }
             }
         )}>
-            <Card style={{ width: '18rem' }} className='m-4'>
+            <Card style={{ width: '18rem' }} className='mb-4'>
                 <Card.Body className='grid justify-items-center'>
-                    <Image src={photo} fluid />
+                    <Image src={photo} className='h-36' />
                     <Card.Title>{brand}</Card.Title>
                     <div className='grid justify-items-center'>
                         <div>
