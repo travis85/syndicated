c73dd2ed462d5d2ba49b0b31ed9ab2533a24c76b
travis flake <tflake83@gmail.com>
2022-11-29 06:16:16 -0500

    added route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header.js';
-import ApparelProductPage from './components/ApparelProductPage.js';
 import PreCart from './components/PreCart';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
@@ -14,6 +13,8 @@ import LoginPage from './components/LoginPage';
 import AddProductPage from './components/AddProductPage';
 import SignIn from './components/SignIn';
 import PartnerProfilePage from './components/PartnerProfilePage';
+import ApparelProductPage from './components/ApparelProductPage';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route exact path='/ApparelProductPage' element={<ApparelProductPage />}/>
         <Route exact path='/SelectedPartner' element={<SelectedPartner />}/>
         <Route exact path='/PreCart' element={<PreCart />} />
+        <Route exact path='/Cart' element={<Cart />} />
         <Route exact path='/LoginPage' element={<LoginPage />} />
         <Route exact path='/RegisterPage' element={<RegisterPage />} />
         <Route exact path='/FoodProductPage' element={<FoodProductPage />} />
@@ -32,7 +34,6 @@ function App() {
         <Route exact path='/AddProductPage' element={<AddProductPage />} />
         <Route exact path='/SignIn' element={<SignIn />} />
         <Route exact path='/PartnerProfilePage' element={<PartnerProfilePage />} />
-
       </Routes>
     </div>
     <Footer/>
