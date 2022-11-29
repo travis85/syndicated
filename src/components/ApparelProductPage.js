c6d38d3d81b355838227b10d9f6fbc677e4779c6
travis flake <tflake83@gmail.com>
2022-11-29 06:17:40 -0500

    css change

--- src/components/ApparelProductPage.js
+++ src/components/ApparelProductPage.js
@@ -7,27 +7,31 @@ export default function ApparelProductPage() {
   partnersInfo = partnersInfo.flat()
   const navigate = useNavigate()
   
-  const fetchPartnersInfo = () => {
-    fetch("/apparalPartners")
+  const fetchPartnersInfo = async() => {
+    await fetch("/apparalPartners")
     .then(res => res.json())
     .then(res => setPartnersInfo( res ))
     .catch(err => err)
   }
 
+  console.log(partnersInfo)
   useEffect(() => {
     fetchPartnersInfo()
-  }, [])
+  },[])
   
   return (
     <>
-      <div >
+      <div className='grid grid-cols-4'>
+        
         {partnersInfo.map((partner) => {
+          const ratings =  partner.reviews.map((rating) => rating.rating) 
+          
             return (
          
-          <div className='w-fit' key={partner.partnerId} >
+          <div className='w-fit ' key={partner.partnerId} >
             <a href='' onClick={() =>
               navigate('/SelectedPartner', { state: { id: partner.partnerId } })}>
-              <FeaturedPartnersCard photo={partner.companyLogoStorageUrl} brandName={partner.companyName} rating={partner.rating} id={partner.partnerId} /> 
+              <FeaturedPartnersCard photo={partner.companyLogoStorageUrl} brandName={partner.companyName} ratingsArray={ratings} id={partner.partnerId} /> 
             </a>
           </div>
         )
