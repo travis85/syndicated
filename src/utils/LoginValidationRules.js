bc382d2400c897347e02efe7f4544cf11535ac7e
travis flake <tflake83@gmail.com>
2022-12-09 16:25:19 -0500

    added  error statments

--- src/utils/LoginValidationRules.js
+++ src/utils/LoginValidationRules.js
@@ -16,8 +16,34 @@ export default function validate(values) {
   } else if (!/[A-Z]/g.test(values.password)) {
     errors.password = "Password must contain atleast 1 capitol letter";
   }
+  if (!values.companyName) {
+    errors.companyName = 'Company Name is Required'
+  }
+  if (!values.city) {
+    errors.city = 'City is Required'
+  }
+  if (!values.zipCode) {
+    errors.zipCode = 'Zip Code is Required'
+  }
+  if (!values.address) {
+    errors.address = 'Address is Required'
+  }
+  if (!values.state) {
+    errors.state = 'State is Required'
+  }
+  if (!values.companyLogo) {
+    errors.companyLogo = 'Logo is Required'
+  }
+  if (!values.contactPerson) {
+    errors.contactPerson = 'Contact Person is Required'
+  }
   if (values.password !== values.password2) {
     errors.password = "Passwords Dont Match!"
   }
   return errors;
 }
+
+
+                
+                
+                
