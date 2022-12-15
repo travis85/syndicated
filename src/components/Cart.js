45bbeaec2a898a37d199a6b7639433bade9edd37
travis flake <tflake83@gmail.com>
2022-12-14 20:07:59 -0500

    cart content

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,40 @@
 import React from 'react'
-
-export default function Cart() {
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/esm/Image';
+import Button from 'react-bootstrap/esm/Button';
+export default function Cart({ cartItems, handleDelete, handleCheckOut }) {
+  const total = cartItems.reduce((acc, obj) => {
+    return acc + obj.total
+  },0)
   return (
-    <div>Cart</div>
+    <div className=' border rounded-md p-4 w-[50%]'>
+          <div className=''>
+            <Table striped >
+              <thead>
+                <tr>
+                  <th>Company</th>
+                  <th>Product</th>
+                  <th>Product Photo</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                { cartItems.map((product) => {
+                  return (
+                    <tr className='' key={product.productId}>
+                      <td>{product.companyName}</td>
+                      <td>{product.quanity}-{product.nameOfItem}   </td>
+                      <td className='grid place-content-center'><Image src={product.photo} className='h-10 place-self-center' /></td>
+                      <td>{`$${product.price}`}</td>
+                      <td><button className='text-red-500' onClick={()=>handleDelete(product.productId)}>Delete</button></td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </Table>
+      </div>
+      Total: ${total}<br></br>
+      <Button onClick={()=> handleCheckOut()}>check out</Button>
+      </div>
   )
 }
