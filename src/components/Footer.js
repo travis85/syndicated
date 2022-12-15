b306bc2300f3fdb39cfba2e9e6ea8f47808abab4
travis flake <tflake83@gmail.com>
2022-12-14 20:06:05 -0500

    register and sign in link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div className='bg-gray-700 h-40'>
-         footer
+    <div className='bg-gray-700 h-40 p-8'>
+      <Link to={'/RegisterPage'} className='no-underline text-white'>Become A partner</Link><br></br>
+      <Link to={'/SignIn'} className='no-underline text-white'>Sign In</Link>
+
     </div>
   )
 }
