82939989f66c8d35295bd3ef3974ddde91559cce
travis flake <tflake83@gmail.com>
2022-12-14 20:06:20 -0500

    add content

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,17 @@
-import React, { useState} from 'react'
+import React from 'react'
 
 export default function Landing() {
-
+     
 
   return (
-    <div>
-      landing
+    <div className='grid place-content-center mt-10'>
+      <h1>This site is a work in progress...</h1>
+      <p>Syndicated is a mock site where small companies can have online commerce amongst other small companies</p>
+      <p>
+        <span className='text-green-700'>****Please play with the with the functionality and add your own company and products <br></br>
+        by clicking on 'Become a Partner' in the footer section****</span>
+      </p>
+    
     </div>
   )
 }
